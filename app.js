836f270db04ddeede61d267b72cc9035dee71974
MaksonGitHub25 <makson94021@gmail.com>
2022-08-17 22:06:48 +0300

    add style.css, done main structure

--- app.js
+++ app.js
@@ -1,5 +1,82 @@
+const btn = document.querySelector('.btn');
 
+alert('Привет пользователь! Это интернет магазин, в котором ты можешь купить любое животное(российского загарбника)');
 
+function menu() {
+    const userFunctionChoosen = prompt(`Какую функцию выберем?
+1) Просмотреть конкретную категорию
+2) Все животные
+3) Добавить животное
+4) Удалить животное
+5) Выйти`);
+
+    checkUserInput(userFunctionChoosen);
+
+    functionChooseHandler(userFunctionChoosen);
+}
+
+function checkUserInput(userFunctionChoosen) {
+    if (/1|2|3|4|5/.test(userFunctionChoosen)) {
+        return;
+    } else {
+        alert('Ты ввел неверное значение!');
+        menu();
+        return;
+    }
+}
+
+function functionChooseHandler(choosenFunc) {
+    const functionArray = [showChoosenCategory, showAllAnimals, addNewAnimal, deleteAnimal, exit];
+
+    functionArray[+choosenFunc - 1]();
+
+    // switch (choosenFunc) {
+    //     case '1':
+    //         showChoosenCategory();
+    //         break;
+        
+    //     case '2':
+    //         showAllAnimals();
+    //         break;
+
+    //     case '3':
+    //         addNewAnimal();
+    //         break;
+
+    //     case '4':
+    //         deleteAnimal();
+    //         break;
+
+    //     case '5':
+    //         exit();
+    //         break;
+    
+    //     default:
+    //         break;
+    // }
+}
+
+function showChoosenCategory() {
+    console.log('showChoosenCategory');
+}
+
+function showAllAnimals() {
+    console.log('showAllAnimals');
+}
+
+function addNewAnimal() {
+    console.log('addNewAnimal');
+}
+
+function deleteAnimal() {
+    console.log('deleteAnimal');
+}
+
+function exit() {
+    alert('Хорошего дня!');
+}
+
+btn.addEventListener('click', menu);
 
 
 
